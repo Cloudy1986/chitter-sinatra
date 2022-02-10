104e607fa3150f81b505ef22a5167d8bce38793b
Mark McLeod <markmcleod45@gmail.com>
2022-02-10 13:39:32 +0000

    Write failing feature test for view peeps

---
+++ spec/features/user_can_view_peeps_spec.rb
@@ -0,0 +1,9 @@
+feature 'View peeps' do
+  scenario 'user can view a list of peeps' do
+    visit '/peeps'
+    expect(page).to have_content 'Latest peeps'
+    expect(page).to have_conent "I'm learning to code and practising building a web app"
+    expect(page).to have_content "Ruby is the best!"
+    expect(page).to have_content "I'm loving using Sinatra to build a web app"
+  end
+end
