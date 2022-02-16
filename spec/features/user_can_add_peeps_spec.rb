35514fe0c1b9015823e1b06c813590adacfdbe30
Mark McLeod <markmcleod45@gmail.com>
2022-02-16 14:13:12 +0000

    Display sign up linked in header if not signed in or add peep link if signed in

--- spec/features/user_can_add_peeps_spec.rb
+++ spec/features/user_can_add_peeps_spec.rb
@@ -2,7 +2,11 @@
 
 feature 'Add peeps' do
   scenario 'user can add a peep and it displays in the list of peeps' do
-    visit '/peeps'
+    visit '/sign-up'
+    fill_in 'username', with: 'Dave MacDonald'
+    fill_in 'email', with: 'dave@example.com'
+    fill_in 'password', with: 'password123'
+    click_button 'Sign up'
     click_link 'Add a new peep'
     fill_in 'message', with: 'This is a new peep'
     click_button 'Add peep'
