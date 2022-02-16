a9f7dd01e510a4a376b41ac86bff093bbac0fb13
Mark McLeod <markmcleod45@gmail.com>
2022-02-16 12:33:42 +0000

    .create method encrypts the user password

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,5 @@
 require 'pg'
+require 'bcrypt'
 
 class User
 
@@ -11,12 +12,13 @@ class User
   end
 
   def self.create(username:, email:, password:)
+    encrypted_password = BCrypt::Password.create(password)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'chitter_sinatra_test')
     else
       connection = PG.connect(dbname: 'chitter_sinatra')
     end
-    result = connection.exec_params("INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, username, email;", [username, email, password])
+    result = connection.exec_params("INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, username, email;", [username, email, encrypted_password])
     User.new(id: result[0]['id'], username: result[0]['username'], email: result[0]['email'])
   end
 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -8,11 +8,16 @@ describe User do
 
       test_connection = PG.connect(dbname: 'chitter_sinatra_test')
       test_result = test_connection.exec_params("SELECT * FROM users WHERE id = $1;", [user.id])
-      
+
       expect(user).to be_a User
       expect(user.id).to eq test_result[0]['id']
       expect(user.username).to eq test_result[0]['username']
       expect(user.email).to eq test_result[0]['email']
+    end
+
+    it "encrypts the password" do
+      expect(BCrypt::Password).to receive(:create).with('password1234')
+      User.create(username: 'Bob', email: 'bob@example.com', password: 'password1234')
     end
   end
 
