b9cf5af42534cecac1b77c29e0f9194a03215fbb
Mark McLeod <markmcleod45@gmail.com>
2022-02-17 13:07:16 +0000

    Fix rubocop offenses

--- spec/features/user_can_delete_peep_spec.rb
+++ spec/features/user_can_delete_peep_spec.rb
@@ -17,7 +17,7 @@ feature 'Delete a peep' do
   scenario 'user cannot delete a peep that belongs to a different user' do
     user = User.create(username: 'Bob', email: 'bob@example.com', password: 'password1234')
     Peep.create(message: 'Bob peep', user_id: user.id)
-    logged_in_user = User.create(username: 'Mary', email: 'mary@example.com', password: '1234password')
+    User.create(username: 'Mary', email: 'mary@example.com', password: '1234password')
     visit '/log-in'
     fill_in 'email', with: 'mary@example.com'
     fill_in 'password', with: '1234password'
