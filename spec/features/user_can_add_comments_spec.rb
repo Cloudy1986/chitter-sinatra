73645cfe3feeef22ce67a2eed6a966933103e199
Mark McLeod <markmcleod45@gmail.com>
2022-02-21 17:38:13 +0000

    Fix failing feature test

--- spec/features/user_can_add_comments_spec.rb
+++ spec/features/user_can_add_comments_spec.rb
@@ -6,7 +6,7 @@ feature 'Add comment' do
     fill_in 'email', with: 'bob@example.com'
     fill_in 'password', with: 'password1234'
     click_button 'Log in'
-    click_button 'Comment'
+    click_button 'Add comment'
     expect(current_path).to eq "/peeps/#{peep.id}/comments/new"
     fill_in 'comment_text', with: 'This is a comment'
     click_button 'Add comment'
